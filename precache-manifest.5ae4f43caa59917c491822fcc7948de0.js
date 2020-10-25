c3293c24a9aef35d4450631ff9210109e6e501f8
Yshiro-HD <hhuamanid@unsa.edu.pe>
2020-10-25 16:08:13 -0500

    primer demo

---
+++ precache-manifest.5ae4f43caa59917c491822fcc7948de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07237d43d54e9f75cc6c1415d3480c6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47b08d811742a2d84c30",
+    "url": "/static/css/main.aa4742b9.chunk.css"
+  },
+  {
+    "revision": "d48240840eebd36a477d",
+    "url": "/static/js/2.387b62f4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.387b62f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b08d811742a2d84c30",
+    "url": "/static/js/main.5c5c868d.chunk.js"
+  },
+  {
+    "revision": "17fda4e2731f5a890c45",
+    "url": "/static/js/runtime-main.7e971750.js"
+  }
+]);
