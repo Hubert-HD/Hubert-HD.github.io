e364739541d303bfd59314dac5aba74be2b37979
Yshiro-HD <hhuamanid@unsa.edu.pe>
2020-12-09 10:23:15 -0500

    septimo demo

---
+++ precache-manifest.c7e48a768ed7b268b055f3c8849d28e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebbcced7298fdc65e0af87bbe886f1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ed5899c2637b641bb68",
+    "url": "/static/css/main.c1f1e6f6.chunk.css"
+  },
+  {
+    "revision": "b1796865d7ce9f3bf432",
+    "url": "/static/js/2.12d2796e.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.12d2796e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ed5899c2637b641bb68",
+    "url": "/static/js/main.16c8c4f4.chunk.js"
+  },
+  {
+    "revision": "74d333d932f437098253",
+    "url": "/static/js/runtime-main.046e65b7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6d860770d97a9b4e977807d96b06972.js"
+  "/precache-manifest.c7e48a768ed7b268b055f3c8849d28e3.js"
 );
 
 self.addEventListener('message', (event) => {
