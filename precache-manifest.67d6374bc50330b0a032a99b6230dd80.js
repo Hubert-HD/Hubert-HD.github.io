dd8b7681080def6c859eda474d5941ffc8096e7f
Yshiro-HD <hhuamanid@unsa.edu.pe>
2020-12-19 19:35:06 -0500

    decimo demo

--- precache-manifest.67d6374bc50330b0a032a99b6230dd80.js
+++ precache-manifest.67d6374bc50330b0a032a99b6230dd80.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd9c80b737a7f6b24fae22a274db5fd5",
+    "revision": "7b1418631cc19d9708ccda41b264fb34",
     "url": "/index.html"
   },
   {
-    "revision": "540e24b71758dce0c179",
+    "revision": "7803630f52613b62505d",
     "url": "/static/css/main.ee7a5740.chunk.css"
   },
   {
-    "revision": "9c962395a075ee395063",
-    "url": "/static/js/2.aa962e7e.chunk.js"
+    "revision": "1b5f4b082f6b10564bd6",
+    "url": "/static/js/2.c6ffbded.chunk.js"
   },
   {
     "revision": "09443f2c1df71aee499e73478b2dd8c3",
-    "url": "/static/js/2.aa962e7e.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.c6ffbded.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "540e24b71758dce0c179",
-    "url": "/static/js/main.f35acba8.chunk.js"
+    "revision": "7803630f52613b62505d",
+    "url": "/static/js/main.dcad1f11.chunk.js"
   },
   {
     "revision": "74d333d932f437098253",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65253301180dc38bbe59a9cde266b51d.js"
+  "/precache-manifest.67d6374bc50330b0a032a99b6230dd80.js"
 );
 
 self.addEventListener('message', (event) => {
